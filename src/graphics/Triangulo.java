f6981420cfc237c7feecb4fa534a71bf7d7ecf62
hanniahaydar <148922406+hanniahaydar@users.noreply.github.com>
2023-11-22 02:57:54 -0600

    modificando métodos

--- src/graphics/Triangulo.java
+++ src/graphics/Triangulo.java
@@ -1,5 +1,8 @@
 package graphics;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 /**
  *
  * @author Héctor Quej Cosgaya
@@ -8,5 +11,32 @@ package graphics;
  * ¡Esta clase necesita tu ayuda!
  */
 public class Triangulo extends FiguraGrafica {
+    private	int[]	x	=	{50,	15,	85};
+private	int[]	y	=	{15,	65,	65};
+
+    @Override
+    public void dibujar(Graphics g) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void encojer(Graphics g) {
+        g.drawPolygon(x,	y,	3);
+}
+
+    @Override
+    public void agrandar(Graphics g) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void cambiarColor(Graphics g, Color c) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void mover(Graphics g, String direccion) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
